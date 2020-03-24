4503dee9c1a2797988ddd37c7482625fa745b19d
Hon Lam <hon@junglescout.com>
2020-03-23 21:12:59 -0700

    Add ES6 compatibility. Flesh out script for migrate-up.

--- Readme.md
+++ Readme.md
@@ -1,5 +1,16 @@
 # Development Guides
 
+## Node
+
+### Should use version 13 to support ES6.
+
+Compatibility with ES6:
+<br/>https://stackoverflow.com/questions/45854169/how-can-i-use-an-es6-import-in-node
+
+### Child processes
+
+https://www.freecodecamp.org/news/node-js-child-processes-everything-you-need-to-know-e69498fe970a/
+
 ## Database
 
 ### node-pg-migrate
--- scripts/migrate-up.js
+++ scripts/migrate-up.js
@@ -1,4 +1,4 @@
-
+import { exec } from 'child_process'
 
 const user = process.env.PGEUSER,
 const host = process.env.PGEHOST,
@@ -8,4 +8,30 @@ const port = process.env.PGPORT
 
 // Example
 // DATABASE_URL=postgres://username:password@host:5432/dateabase_name npm run migrate up"
-const command = `DATABASE_URL=postgres://${user}:${password}@${host}:${port}/${database}`
+const command = `DATABASE_URL=postgres://${user}:${password}@${host}:${port}/${database} npm run migrate up`
+
+exec(command, (err, stdout, stderr) => {
+  if (err) {
+    console.error(`
+      Error not from stderr:
+      ---
+      ${err}
+    `)
+  }
+
+  if (stdout) {
+    console.log(`
+      Running 'DATABASE_URL=postgres://username:password@host:PORT_NUMBER/dateabase_name npm run migrate up':
+      ---
+      ${stdout}
+    `)
+  }
+
+  if (stderr) {
+    console.error(`
+      STDERR:
+      ---
+      ${stderr}
+    `)
+  }
+})
