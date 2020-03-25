e57afb0ff3692d433e9055aab0bc149a39083bb9
Hon Lam <hon@junglescout.com>
2020-03-24 23:19:55 -0700

    Fix migration file by fixing Javascript modules.

---
+++ migrations/1584781139705_expenses-and-income-sources.cjs
@@ -0,0 +1,53 @@
+exports.shorthands = undefined
+
+exports.up = function up(pgm) {
+  const periodUnitsDefault = function(tableName) {
+    return {
+      expenses: 'months',
+      income_sources: 'years'
+    }[tableName]
+  }
+
+  const commonColumns = function(tableName) {
+    const columns = {
+      id: 'id',
+      name: {
+        type: 'text',
+        unique: true,
+        notNull: true
+      },
+      description: {
+        type: 'text',
+        unique: true,
+        notNull: true
+      },
+      amount: {
+        type: 'numeric',
+        notNull: true,
+        default: 0
+      },
+      period_length: {
+        type: 'numeric',
+        notNull: true,
+        default: 1
+      },
+      period_units: {
+        type: 'text',
+        check: "(period_units IN ('days', 'weeks', 'months', 'years'))",
+        default: periodUnitsDefault(tableName)
+      },
+      createdAt: {
+        type: 'timestamp',
+        notNull: true,
+        default: pgm.func('current_timestamp')
+      }
+    }
+
+    return columns
+  }
+
+  pgm.createTable(
+    'expenses',
+    commonColumns('expenses')
+  )
+}
