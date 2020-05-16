f0cd7a6e8361737fff70d7eb668cb1fecb281808
Hon Lam <hon@junglescout.com>
2020-05-16 15:00:48 -0700

    Encryption service module. Query for finding foreign keys.

---
+++ app/services/encryption.js
@@ -0,0 +1,31 @@
+import { envVariables } from '../../config.js'
+
+const ENCRYPTION_ALGORITHM = envVariables.ENCRYPTION_ALGORITHM
+const UNENCRYPTED_ENCODING = 'utf8'
+const ENCRYPTED_ENCODING = 'hex'
+const ENCRYPTION_KEY = envVariables.ENCRYPTION_KEY
+const ENCRYPTION_IV = envVariables.ENCRYPTION_IV
+
+export const encrypt = toEncrypt => {
+  const cipher = crypto.createCipheriv(
+    ENCRYPTION_ALGORITHM,
+    ENCRYPTION_KEY,
+    ENCRYPTION_IV
+  )
+
+  let encrypted = cipher.update(toEncrypt, UNENCRYPTED_ENCODING, ENCRYPTED_ENCODING)
+  encrypted += cipher.final(ENCRYPTED_ENCODING)
+  return encrypted
+}
+
+export const ecrypt = toDecrypt => {
+  const decipher = crypto.createDecipheriv(
+    ENCRYPTION_ALGORITHM,
+    ENCRYPTION_KEY,
+    ENCRYPTION_IV
+  )
+
+  let decrypted = decipher.update(toDecrypt, ENCRYPTED_ENCODING, UNENCRYPTED_ENCODING)
+  decrypted += decipher.final(UNENCRYPTED_ENCODING)
+  return decrypted
+}
