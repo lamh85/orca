f113ba3957bb2dad174870354eec8713aa685850
Hon Lam <hon@junglescout.com>
2020-05-23 23:05:50 -0700

    Add comment for an environment variable.

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ const envVariables = {
   APP_PORT: process.env.APP_PORT,
   ENCRYPTION_ALGORITHM: process.env.ENCRYPTION_ALGORITHM,
   ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
+  // Must be 128 bytes long
   ENCRYPTION_IV: process.env.ENCRYPTION_IV,
   PG_USER: process.env.PG_USER,
   PG_HOST: process.env.PG_HOST,
